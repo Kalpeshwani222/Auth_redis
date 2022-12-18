05fa810152f879f89394e7c528c03717d0afc451
Kalpesh Wani <wanikalpeshanil@gmail.com>
2022-12-18 22:26:47 +0530

    hash the password

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
 
 
 const userSchema = new Schema({
@@ -15,6 +16,30 @@ const userSchema = new Schema({
         required: true,
     }
 })
+
+
+userSchema.pre('save',async function (next){
+    try {
+        // console.log("call before saving a user");
+        const salt = await bcrypt.genSalt(10);
+        // console.log(this.email,this.password);
+        const hashPass = await bcrypt.hash(this.password, salt);
+        this.password = hashPass;
+        next()
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+// userSchema.post('save',async function (next){
+//     try {
+//         console.log("call after saving a user");
+//     } catch (error) {
+//         next(error)
+//     }
+// })
+
 
 const User = mongoose.model('user',userSchema);
 module.exports = User;
