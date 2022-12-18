6fc478cb17202a62b68380c62320aa0527c9165b
Kalpesh Wani <wanikalpeshanil@gmail.com>
2022-12-18 22:00:26 +0530

    add the Joi validations

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,24 +2,30 @@ const express = require('express');
 const createError = require('http-errors');
 const router = express.Router();
 const User = require("../models/user.model");
+const {authSchema} = require("../helpers/validate_schema");
+
 
 router.post('/register',async(req,res,next)=>{
    try {
-       const { email, password } = req.body;
-        if(!email || !password){
-            throw createError.BadRequest();
-        }
+    //    const { email, password } = req.body;
+        // if(!email || !password){
+        //     throw createError.BadRequest();
+        // }
+        const result = await authSchema.validateAsync(req.body);
         
-        const alreadyExist = await User.findOne({email: email});
+        const alreadyExist = await User.findOne({email: result.email});
         if(alreadyExist){
-            throw createError.Conflict(`${email} is already exists`)
+            throw createError.Conflict(`${result.email} is already exists`)
         }
 
-        const user = new User({email, password});
+        const user = new User(result);
         const saveUser = await user.save();
 
         res.send(saveUser);
    } catch (error) {
+       if(error.isJoi === true){
+           error.status = 422
+       }
        next(error)
    } 
 })
